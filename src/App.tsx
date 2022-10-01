7ede11c05a2f495071fb2f8d5864c3d27b64d781
alexsharich <alexsharich@gmail.com>
2022-10-01 15:51:39 +0200

    clean code

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,21 @@ import { WeatherInformation } from './ui/form/weatherInformation/WeatherInformat
 
 function App() {
 
-  let [cityName, setCityName] = useState('')
+let [cityName,setCityName]=useState('')
 
-  const getCurrentWeather = async (e: any) => {
-    e.preventDefault()
-    const city = cityName === '' ? 'London' : cityName
-    const api_url = await
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f56a8950eaa1448bf0e4dd0a329053a9`)
-    const data = await api_url.json()
-    console.log(data)
-  }
+ const getCurrentWeather = async (e:any)=>{
+  e.preventDefault()
+  const city = cityName === '' ? 'London' : cityName
+  const api_url = await
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f56a8950eaa1448bf0e4dd0a329053a9`)
+  const data = await api_url.json()
+  console.log(data)
+}
 
   return (
     <div className="App">
-      <Form getCurrentWeather={getCurrentWeather} setCityName={setCityName} />
-      <WeatherInformation />
+           <Form getCurrentWeather={getCurrentWeather} setCityName={setCityName}/>
+           <WeatherInformation />
     </div>
   );
 }
